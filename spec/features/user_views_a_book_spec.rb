59bbbdf8b2278237450cf287afc8f3a7ea49d1a0
Griff Hall <griffhall1@gmail.com>
2014-07-06 22:36:02 -0400

    added tests

---
+++ spec/features/user_views_a_book_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature "user visits a book's details page" do
+  # Acceptance Criteria
+  #
+  # * I see a title that let's me know I'm on the book's page
+  # * I see all reviews
+
+  it "displays a title" do
+    visit book_path(book)
+
+    expect(page).to have_content "Add a Review"
+    expect(page).to have_content 'Back to Books'
+  end
+end
---
+++ spec/features/user_views_list_of_books_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+
+feature "user visits the books page" do
+  #acceptance criteria:
+  #
+  # * I see a title that lets me know I'm on the right page
+  # * I see all of the books listed
+
+  it "displays a title" do
+    visit '/books'
+    expect(page).to have_content "All Books"
+  end
+
+  it "sees all the books listed on the page" do
+    # Create two books so we can test that they're displayed on the page
+    book1 = Book.create!(title: "Look Book", author: "hemmingway", year: "1951")
+    book2 = Book.create!(title: "To Kill a Mockingbird", author: "Harper Lee", year: "1961")
+
+    visit 'books'
+
+    expect(page).to have_content book1.title
+    expect(page).to have_content book2.title
+end
